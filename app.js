09c4fd7f271993b3e5549de3e8e92a6f9864b224
Alex Langshur <>
2018-05-20 22:02:24 -0700

    updates with express and node js

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-/* starting location for website
- * 
- * LOAD HTML HOMEPAGE (index.html)
- * 
- * LOAD SERVER (server.js)
- */
+// init app
+const express = require('express');
+const path = require('path');
+const bodyParser= require('body-parser'); 
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/front'));
+app.use('/images', express.static(__dirname + '/images'));
+
+app.listen(3000, function() {
+  console.log('Listening on 3000');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/front/index.html');
+});
+
+app.post('/', (req, res) => {
+  console.log('Hellooooooooooooooooo!');
+})
